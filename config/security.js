132d392a3c019f48dd2c76c5f9ab50cc1b386fe8
Triyys <ngoductri123@gmail.com>
2022-10-03 10:38:19 +0700

    add cors https://foodmaze.herokuapp.com

--- config/security.js
+++ config/security.js
@@ -1,6 +1,6 @@
 module.exports = {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://foodmaze.herokuapp.com'],
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization'],
         exposedHeaders: ['Location'],
