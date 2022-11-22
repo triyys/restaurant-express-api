2f6c1517448975d967d9f4cfba18e7925ca81685
Triyys <ngoductri123@gmail.com>
2022-11-22 17:52:29 +0700

    add WEBSITE_BASE_URL env to cors

--- config/security.js
+++ config/security.js
@@ -1,6 +1,6 @@
 module.exports = {
     cors: {
-        origin: ['http://localhost:3000', 'https://foodmaze.herokuapp.com'],
+        origin: ['http://localhost:3000', 'https://foodmaze.herokuapp.com', process.env.WEBSITE_BASE_URL],
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization'],
         exposedHeaders: ['Location'],
